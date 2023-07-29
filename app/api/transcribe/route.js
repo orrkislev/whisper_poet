e5c1813ce42fad43eb65c04cd985dbfbfa62a75a
Orr Kislev <orrkislev@gmail.com>
2023-07-29 16:02:40 +0300

    .

--- app/api/transcribe/route.js
+++ app/api/transcribe/route.js
@@ -1,14 +1,20 @@
-export async function POST(request) {
-    const body = await request.formData();
-    const res = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+export async function POST(req) {
+  if (req.method === 'POST') {
+    const body = await req.formData();
+    const fetchRes = await fetch("https://api.openai.com/v1/audio/transcriptions", {
       headers: {
         Authorization: `Bearer ${process.env.API_KEY}`,
       },
       method: "POST",
       body: body,
     });
-    const json = await res.json();   
-    return {
-        body: json.text,
-    };
+    const json = await fetchRes.json();
+    console.log(json);
+    return new Response(JSON.stringify(json), {
+      status: 200,
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    });
+  }
 }
