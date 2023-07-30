9a0d3fb30030bfa9c7a31a0535dd4918b349a169
Orr Kislev <orrkislev@gmail.com>
2023-07-30 11:05:01 +0300

    .

---
+++ main.js
@@ -0,0 +1,92 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    mic = new p5.AudioIn();
+    mic.start();
+
+    initHark();
+    initVis()
+
+    speaking = false
+}
+
+function mousePressed() {
+    getAudioContext().resume()
+}
+
+async function initHark() {
+    stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    var options = {};
+    var speechEvents = hark(stream, options);
+
+    const recorder = new MediaRecorder(stream);
+    const audioChunks = []
+    recorder.start();
+
+    function resetRecording(resetSpeaking = false) {
+        if (resetSpeaking) speaking = false
+        if (!speaking) {
+            startRecordData()
+            audioChunks.length = 0;
+            recorder.stop();
+            recorder.start();
+        }
+    }
+
+    async function endRecording() {
+        const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+        sendToWhisper(audioBlob)
+        await visRecording()
+
+        resetRecording(true)
+
+        // const audioUrl = URL.createObjectURL(audioBlob);
+        // const audio = new Audio(audioUrl);
+        // audio.play();
+    }
+
+    recorder.addEventListener("stop", () => {
+        if (!speaking) return
+        endRecording()
+    });
+
+
+    ticker = setInterval(resetRecording, 500)
+    recorder.addEventListener("dataavailable", event => { if (speaking) audioChunks.push(event.data) });
+    speechEvents.on('speaking', () => speaking = true);
+    speechEvents.on('stopped_speaking', () => recorder.stop());
+}
+
+async function sendToWhisper(blob) {
+    const file = new File([blob], "audio.wav", { type: "audio/wav" });
+
+    const data = new FormData();
+    data.append("file", file);
+    data.append("model", "whisper-1");
+    data.append("language", "he");
+
+    const res = await fetch("https://whisper-poet.vercel.app/api/transcribe", {
+        method: "POST",
+        body: data,
+    });
+
+    const json = await res.json()
+    const txt = json.text
+
+    print(txt)
+
+    textSize(20)
+    const textW = textWidth(txt)
+    const newTextSize = width / textW * 20
+    textSize(newTextSize)
+    textAlign(CENTER, CENTER);
+    fill(0)
+    noStroke()
+    text(txt, width / 2, height / 2)
+}
+
+
+
+function draw() {
+    visLive();
+}
---
+++ visLive.js
@@ -0,0 +1,98 @@
+function initVis() {
+    fft = new p5.FFT();
+    fft.setInput(mic);
+
+    level = 0
+    pitch = 0
+
+    recordData = []
+}
+
+function startRecordData() {
+    recordData = []
+    recordStart = Date.now()
+}
+
+function visRecording() {
+    background(255)
+    const recordingTime = (Date.now() - recordStart) / 1000
+
+    const minLevel = recordData.reduce((a, b) => a.level < b.level ? a : b).level
+    const maxLevel = recordData.reduce((a, b) => a.level > b.level ? a : b).level
+    const minPitch = recordData.reduce((a, b) => a.pitch < b.pitch ? a : b).pitch
+    const maxPitch = recordData.reduce((a, b) => a.pitch > b.pitch ? a : b).pitch
+
+    noFill()
+    stroke(0)
+    beginShape()
+    for (let i = 0; i < recordData.length; i++) {
+        const x = width * i / recordData.length
+        const y = map(recordData[i].level, minLevel, maxLevel, 0, height * .1)
+        vertex(x, height * .1 + y);
+    }
+    endShape()
+
+    beginShape()
+    for (let i = 0; i < recordData.length; i++) {
+        const x = width * i / recordData.length
+        const y = map(recordData[i].pitch, minPitch, maxPitch, 0, height * .1)
+        vertex(x, height * .2 + y);
+    }
+    endShape()
+}
+
+function visLive() {
+    fill(200)
+    noStroke()
+    rect(0, height, width, -height * .2)
+
+    const newLevel = mic.getLevel();
+    level = lerp(level, newLevel, 0.5);
+
+    const spectrum = fft.analyze();
+    const relevant = spectrum.filter((_, i) => i < 120);
+    const newPitch = relevant.indexOf(spectrum.reduce((a, b) => a > b ? a : b));
+    pitch = lerp(pitch, newPitch, 0.5);
+
+    recordData.push({ level, pitch })
+
+    stroke(0)
+    // strokeWeight(width / spectrum.length)
+    for (let i = 0; i < spectrum.length; i++) {
+        const x = i * 3
+        const h = map(spectrum[i], 0, 255, 0, height * .2);
+        line(x, height, x, height - h)
+    }
+
+
+    fill(255)
+    noStroke()
+    circle(width * .75, height * .9, level * height)
+    rect(pitch * 3, height, 3, -height * .2)
+
+    let waveform = fft.waveform();
+    let rms = 0
+    stroke(255)
+    strokeWeight(2)
+    noFill()
+    beginShape()
+    for (let i = 0; i < waveform.length; i++) {
+        let x = map(i, 0, waveform.length, 0, width);
+        let y = waveform[i] * height * .1
+        vertex(x, height * .9 + y);
+
+        rms += waveform[i] * waveform[i]
+    }
+    endShape()
+    rms = sqrt(rms / waveform.length)
+
+    fill(255)
+    noStroke()
+    rect(0, 0, rms * width, 10)
+
+    if (speaking) {
+        fill(255, 0, 0)
+        noStroke()
+        circle(width - 10, height * .8 + 10, 10)
+    }
+}
